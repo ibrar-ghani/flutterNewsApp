05abf248ef0d424508bb8558d7b969141261b765
ImranKhan <ibrar.ghani.aaur@gmail.com>
2024-07-25 10:29:29 +0500

    "Apply loader on the profile page.."

--- lib/src/controllers/profile_controller.dart
+++ lib/src/controllers/profile_controller.dart
@@ -11,6 +11,7 @@ class ProfileController extends GetxController {
   RxString phoneNumber = ''.obs;
   RxString address = ''.obs;
   RxString profilePictureUrl = ''.obs;
+  RxBool isLoading = false.obs; // Add a loading state
   final Logger _logger = Logger();
 
   User? get user => FirebaseAuth.instance.currentUser;
@@ -44,6 +45,7 @@ class ProfileController extends GetxController {
 
   Future<void> fetchUserData() async {
     try {
+      isLoading.value = true;
       if (user != null) {
         _logger.i('Fetching user data for email: ${user!.email}');
         setEmail(user!.email ?? '');
@@ -52,11 +54,14 @@ class ProfileController extends GetxController {
       }
     } catch (error) {
       _logger.e('Error fetching user data: $error');
+    } finally {
+      isLoading.value = false;
     }
   }
 
   Future<void> fetchAdditionalUserData() async {
     try {
+      isLoading.value = true;
       if (user != null) {
         final DocumentSnapshot<Map<String, dynamic>> snapshot =
             await FirebaseFirestore.instance
@@ -79,58 +84,63 @@ class ProfileController extends GetxController {
       }
     } catch (error) {
       _logger.e('Error fetching additional user data: $error');
+    } finally {
+      isLoading.value = false;
     }
   }
 
-Future<void> uploadProfilePicture(File file) async {
-  try {
-    if (user != null) {
-      // Verify the file exists before attempting to upload
-      if (!await file.exists()) {
-        _logger.e('File does not exist at path: ${file.path}');
-        throw Exception('File does not exist');
-      }
+  Future<void> uploadProfilePicture(File file) async {
+    try {
+      isLoading.value = true;
+      if (user != null) {
+        // Verify the file exists before attempting to upload
+        if (!await file.exists()) {
+          _logger.e('File does not exist at path: ${file.path}');
+          throw Exception('File does not exist');
+        }
 
-      // Reference to Firebase Storage
-      final storageRef = FirebaseStorage.instance.ref();
-      final profilePicRef = storageRef.child('profile_pictures/${user!.uid}.jpg');
+        // Reference to Firebase Storage
+        final storageRef = FirebaseStorage.instance.ref();
+        final profilePicRef = storageRef.child('profile_pictures/${user!.uid}.jpg');
 
-      _logger.i('Uploading file to: ${profilePicRef.fullPath}');
+        _logger.i('Uploading file to: ${profilePicRef.fullPath}');
 
-      // Upload task
-      final uploadTask = profilePicRef.putFile(file);
+        // Upload task
+        final uploadTask = profilePicRef.putFile(file);
 
-      uploadTask.snapshotEvents.listen((TaskSnapshot snapshot) {
-        _logger.i('Upload progress: ${snapshot.bytesTransferred}/${snapshot.totalBytes}');
-      });
+        uploadTask.snapshotEvents.listen((TaskSnapshot snapshot) {
+          _logger.i('Upload progress: ${snapshot.bytesTransferred}/${snapshot.totalBytes}');
+        });
 
-      final snapshot = await uploadTask.whenComplete(() {
-        _logger.i('Upload completed');
-      });
+        final snapshot = await uploadTask.whenComplete(() {
+          _logger.i('Upload completed');
+        });
 
-      if (snapshot.state == TaskState.success) {
-        _logger.i('Upload successful');
-        final downloadUrl = await snapshot.ref.getDownloadURL();
-        _logger.i('Download URL: $downloadUrl');
+        if (snapshot.state == TaskState.success) {
+          _logger.i('Upload successful');
+          final downloadUrl = await snapshot.ref.getDownloadURL();
+          _logger.i('Download URL: $downloadUrl');
 
-        // Update Firestore with the new URL
-        await FirebaseFirestore.instance
-            .collection('users')
-            .doc(user!.uid)
-            .update({'profilePictureUrl': downloadUrl});
+          // Update Firestore with the new URL
+          await FirebaseFirestore.instance
+              .collection('users')
+              .doc(user!.uid)
+              .update({'profilePictureUrl': downloadUrl});
 
-        setProfilePictureUrl(downloadUrl);
+          setProfilePictureUrl(downloadUrl);
+        } else {
+          _logger.e('Upload failed with state: ${snapshot.state}');
+          throw Exception('Upload failed with state: ${snapshot.state}');
+        }
       } else {
-        _logger.e('Upload failed with state: ${snapshot.state}');
-        throw Exception('Upload failed with state: ${snapshot.state}');
+        _logger.e('User is not authenticated');
+        throw Exception('User is not authenticated');
       }
-    } else {
-      _logger.e('User is not authenticated');
-      throw Exception('User is not authenticated');
+    } catch (error) {
+      _logger.e('Error during upload: $error');
+      throw Exception('Error during upload: $error');
+    } finally {
+      isLoading.value = false;
     }
-  } catch (error) {
-    _logger.e('Error during upload: $error');
-    throw Exception('Error during upload: $error');
   }
-}
 }
--- lib/src/views/profile_page.dart
+++ lib/src/views/profile_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:routingexample/src/controllers/profile_controller.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:routingexample/src/controllers/profile_controller.dart';
 
 class ProfilePage extends StatelessWidget {
   ProfilePage({super.key});
@@ -26,34 +26,40 @@ class ProfilePage extends StatelessWidget {
         foregroundColor: Colors.white,
         backgroundColor: Colors.blueAccent,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // Profile picture
-              GestureDetector(
-                onTap: _pickImage,
-                child: Obx(() {
-                  return CircleAvatar(
+      body: Obx(() {
+        if (profileController.isLoading.value) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Profile picture
+                GestureDetector(
+                  onTap: _pickImage,
+                  child: CircleAvatar(
                     radius: 150.0,
                     backgroundImage: profileController.profilePictureUrl.isNotEmpty
                         ? NetworkImage(profileController.profilePictureUrl.value) as ImageProvider<Object>?
                         : const AssetImage('assets/human-icon-png-1901.png'),
-                  );
-                }),
-              ),
-              const SizedBox(height: 16),
-              Obx(() => Text('Email: ${profileController.email}')),
-              Obx(() => Text('Name: ${profileController.name}')),
-              Obx(() => Text('Phone Number: ${profileController.phoneNumber}')),
-              Obx(() => Text('Address: ${profileController.address}')),
-            ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Text('Email: ${profileController.email}'),
+                Text('Name: ${profileController.name}'),
+                Text('Phone Number: ${profileController.phoneNumber}'),
+                Text('Address: ${profileController.address}'),
+              ],
+            ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
